26363e761f6a5d774dcdb9d6b64d5eae310760a7
EFN <edson.freire@gmail.com>
2018-12-03 12:38:47 -0200

    Mais codigo...

--- src/Account.java
+++ src/Account.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Account {
-}
+    private String name; // variável de instância
+
+    // método para definir o nome no objeto
+    public void setName(String name) {
+        this.name = name; // armazena o nome
+    }
+    // método para recuperar o nome do objeto
+    public String getName() {
+        return name; // retorna valor do nome para o chamador
+    }
+} // fim da classe Account
--- src/Comparison.java
+++ src/Comparison.java
@@ -22,6 +22,7 @@ public class Comparison {
 
         if (number1 < number2)
             System.out.printf("%d < %d%n", number1, number2);
+
         if (number1 > number2)
             System.out.printf("%d > %d%n", number1, number2);
 
